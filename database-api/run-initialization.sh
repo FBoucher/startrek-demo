84ad59d0097462260b1b96521eeb749ae07e1d99
Frank <fboucher@outlook.com>
2024-04-22 19:22:47 -0400

    Security improved

--- database-api/run-initialization.sh
+++
@@ -1,5 +0,0 @@
-/opt/mssql/bin/sqlservr
-# Wait to be sure that SQL Server is up
-sleep 30
-# Execute setup script to create the DB and Data
-/opt/mssql-tools/bin/sqlcmd -U sa -P $MSSQL_SA_PASSWORD -d master -i startrek.sql
