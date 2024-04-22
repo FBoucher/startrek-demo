ba282b852827fe155afb7e777319a5ae86448d5e
Frank <fboucher@outlook.com>
2024-04-22 18:43:31 -0400

    its working

--- database-api/run-initialization.sh
+++ database-api/run-initialization.sh
@@ -1,5 +1,5 @@
+/opt/mssql/bin/sqlservr
 # Wait to be sure that SQL Server is up
-sleep 30s
-
+sleep 30
 # Execute setup script to create the DB and Data
-/opt/mssql-tools/bin/sqlcmd -S sqltrek -U sa -P 1rootP@ssword -d master -i startrek.sql
+/opt/mssql-tools/bin/sqlcmd -U sa -P $MSSQL_SA_PASSWORD -d master -i startrek.sql
