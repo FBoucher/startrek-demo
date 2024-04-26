3a59cba86b673eb76e30466cbaad21043407a6a9
Frank <fboucher@outlook.com>
2024-04-26 11:10:24 -0400

    move to web subFolder

---
+++ src/web/Program.cs
@@ -0,0 +1,32 @@
+using startrekdemo.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000/api/") });
+
+builder.Services.AddStartrekClient()
+    .ConfigureHttpClient(client => client.BaseAddress = new Uri("http://localhost:5000/graphql"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
