d59a5c283c8f7bb91050fa3db7de1020294cb5e4
Frank <fboucher@outlook.com>
2024-06-12 18:26:34 -0400

    Adding basic blazor demo

---
+++ src/web/Program.cs
@@ -0,0 +1,30 @@
+using startrekdemo.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddStartrekClient()
+    .ConfigureHttpClient(client => client.BaseAddress = new Uri("http://localhost:5000/graphql"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
